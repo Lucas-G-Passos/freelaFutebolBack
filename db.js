d397b6516851301ff71e114e1079389a4cc59558
Username-MB <lucasgodpassos@gmail.com>
2025-04-29 15:07:46 -0300

    db changed variables

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
-import mysql from 'mysql2/promise';
-import { configDotenv } from 'dotenv';
+import mysql from "mysql2/promise";
+import { configDotenv } from "dotenv";
 configDotenv();
 
 const db = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DB,
-    waitForConnections: true,
-    connectionLimit: 15,
-    queueLimit: 0,
-    charset: 'utf8mb4_unicode_ci', // Combined charset and collation
-    timezone: 'local',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 15,
+  queueLimit: 0,
+  charset: "utf8mb4_unicode_ci", // Combined charset and collation
+  timezone: "local",
 });
-
 
 export default db;
