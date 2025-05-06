0af6def85a70c2f9223ae889d6c9924a43333a7e
Username-MB <lucasgodpassos@gmail.com>
2025-05-05 21:34:44 -0300

    changed var names

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ import { configDotenv } from "dotenv";
 configDotenv();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 15,
   queueLimit: 0,
