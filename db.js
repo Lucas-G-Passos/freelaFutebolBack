c8b5f7bfe803acf1f2818ae854bff665578a8c35
Username-MB <lucasgodpassos@gmail.com>
2025-05-05 21:39:47 -0300

    changed var

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const db = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 15,
